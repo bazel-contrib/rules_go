0141ae1a2be5859c6276eeaeab53979dbee6dbfc
Andrei Reshetkov <reshetkov@avride.ai>
2025-09-03 19:48:58 +0000

    undo sleep

--- tests/core/cgo/cc_static_init/test_side_effect.cpp
+++ tests/core/cgo/cc_static_init/test_side_effect.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <thread>
 
 #include "tests/core/cgo/cc_static_init/lib.h"
 
 int main() {
-  // We could be too fast. Give the side_effect some time to complete
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
   const int expected = 42;
   const int actual = *GetValue();
   if (actual == expected) {
--- tests/core/cgo/cc_static_init/test_side_effect.go
+++ tests/core/cgo/cc_static_init/test_side_effect.go
@@ -2,15 +2,9 @@ package ccstaticinit
 
 // #include "tests/core/cgo/cc_static_init/lib.h"
 import "C"
-
-import (
-	"testing"
-	"time"
-)
+import "testing"
 
 func TestValue(t *testing.T) {
-	// We could be too fast. Give the side_effect some time to complete
-	time.Sleep(10 * time.Millisecond)
 	const expected = 42
 	actual := int(*C.GetValue())
 	if expected != actual {
